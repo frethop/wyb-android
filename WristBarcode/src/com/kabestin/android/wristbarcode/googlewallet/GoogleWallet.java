150d78193d0929d6198a3e58c50eecfd30d9881f
Mike Jipping <jipping@hope.edu>
2015-06-18 09:47:52 -0400

    Worked on Wallet

--- WristBarcode/src/com/kabestin/android/wristbarcode/googlewallet/GoogleWallet.java
+++ WristBarcode/src/com/kabestin/android/wristbarcode/googlewallet/GoogleWallet.java
@@ -1,6 +1,5 @@
 package com.kabestin.android.wristbarcode.googlewallet;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -8,84 +7,50 @@ import java.io.Reader;
 import java.io.Writer;
 import java.nio.charset.Charset;
 import java.security.GeneralSecurityException;
+import java.security.interfaces.RSAPrivateKey;
+
+import android.content.Context;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.LowLevelHttpRequest;
+import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.JsonGenerator;
 import com.google.api.client.json.JsonParser;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.SecurityUtils;
 
 public class GoogleWallet {
+	
+	public static final HttpTransport httpTransport = new NetHttpTransport();
+	public static final JsonFactory jsonFactory = new GsonFactory();
 
 	GoogleCredential credential;
+	Context source;
 	
-	public GoogleWallet() {
-		
-//		HttpTransport httpTransport = new HttpTransport() {
-//			
-//			@Override
-//			protected LowLevelHttpRequest buildRequest(String arg0, String arg1)
-//					throws IOException {
-//				// TODO Auto-generated method stub
-//				return null;
-//			}
-//		};
-//		JsonFactory jsonFactory = new JsonFactory() {
-//			
-//			@Override
-//			public JsonParser createJsonParser(InputStream arg0, Charset arg1) 
-//					throws IOException {
-//				// TODO Auto-generated method stub
-//				return null;
-//			}
-//			
-//			@Override
-//			public JsonParser createJsonParser(Reader arg0) throws IOException {
-//				// TODO Auto-generated method stub
-//				return null;
-//			}
-//			
-//			@Override
-//			public JsonParser createJsonParser(String arg0) throws IOException {
-//				// TODO Auto-generated method stub
-//				return null;
-//			}
-//			
-//			@Override
-//			public JsonParser createJsonParser(InputStream arg0) throws IOException {
-//				// TODO Auto-generated method stub
-//				return null;
-//			}
-//			
-//			@Override
-//			public JsonGenerator createJsonGenerator(OutputStream arg0, Charset arg1)
-//					throws IOException {
-//				// TODO Auto-generated method stub
-//				return null;
-//			}
-//			
-//			@Override
-//			public JsonGenerator createJsonGenerator(Writer arg0) throws IOException {
-//				// TODO Auto-generated method stub
-//				return null;
-//			}
-//		};
-//		
-//		try {
-//			credential = new GoogleCredential.Builder().setTransport(httpTransport)
+	public GoogleWallet(Context source) {
+				
+		try {
+			InputStream p12Stream = source.getAssets().open("wyb.p12");
+			RSAPrivateKey serviceAccountPrivateKey = 
+					(RSAPrivateKey) SecurityUtils.loadPrivateKeyFromKeyStore(
+							SecurityUtils.getPkcs12KeyStore(), p12Stream, "notasecret",
+							"privatekey", "notasecret");
+//			credential = new GoogleCredential.Builder()
+//					  .setTransport(httpTransport)
 //					  .setJsonFactory(jsonFactory)
-//					  .setServiceAccountId("ServiceAccountEmail@developer.gserviceaccount.com")
+//					  .setServiceAccountId("1034771164092-hc1ffppjbh8icnrvn5b7uqt26k2vrhp5@developer.gserviceaccount.com")
 //					  .setServiceAccountScopes("https://www.googleapis.com/auth/wallet_object.issuer")
-//					  .setServiceAccountPrivateKeyFromP12File(new File("/example/path/to/yourp12file.p12"))
+//					  .setServiceAccountPrivateKey(serviceAccountPrivateKey)
 //					  .build();
-//		} catch (GeneralSecurityException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		} catch (GeneralSecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
